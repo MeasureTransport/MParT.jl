7acf9610960dcc79b21d545a502db22bf092f1e4
Documenter.jl <documenter@juliadocs.github.io>
2022-07-26 03:05:19 +0000

    build based on d0e54f2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MParT","category":"page"},{"location":"#MParT","page":"Home","title":"MParT","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MParT.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MParT]","category":"page"}]
+}
