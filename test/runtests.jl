b142d3aa38686769181712674ec987b5a02afb69
Danny Sharp <dannys4@mit.edu>
2022-11-16 09:38:25 -0500

    Remove verbosity while testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,13 @@
 using Test
 
-@testset "MParT.jl" verbose=true begin
-    @testset verbose=true "Affine, Composed Map" begin
+@testset "MParT.jl" begin
+    @testset "Affine, Composed Map" begin
         include("mapTypeTest.jl")
     end
-    @testset verbose=true "Monotone Least Squares" begin
+    @testset "Monotone Least Squares" begin
         include("MLS.jl")
     end
-    @testset verbose=true "2D Banana From Samples" begin
+    @testset "2D Banana From Samples" begin
         include("banana2D.jl")
     end
 end
