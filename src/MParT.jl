c3ac64a586b06212aa6f9d3445a461a372364bad
dannys4 <dannys4@vt.edu>
2023-07-03 17:44:33 -0600

    Work on MParT docs and ATM

--- src/MParT.jl
+++ src/MParT.jl
@@ -1,80 +1,279 @@
 # Wrapping code to make the Julia module usable
 module MParT
-    using CxxWrap
-    using MParT_jll
-    import Libdl
-    @wrapmodule libmpartjl :MParT_julia_module
-    import Base: getindex, lastindex
+using CxxWrap
+using MParT_jll
+import Libdl
+@wrapmodule libmpartjl :MParT_julia_module
+import Base: getindex, lastindex, show, iterate
 
     mapSubtypeAlias{T} = Union{T,<:CxxWrap.CxxWrapCore.SmartPointer{T}}
 
-    function __init__()
-        @initcxx
-        threads = get(ENV, "KOKKOS_NUM_THREADS", nothing)
-        opts = isnothing(threads) ? [] : ["kokkos_num_threads", threads]
-        length(opts) > 0 && @info "Using MParT options: "*string(string.(opts))
-        Initialize(StdVector(StdString.(opts)))
-    end
+function __init__()
+    @initcxx
+    threads = get(ENV, "KOKKOS_NUM_THREADS", nothing)
+    opts = isnothing(threads) ? [] : ["kokkos_num_threads", threads]
+    length(opts) > 0 && @info "Using MParT options: "*string(string.(opts))
+    Initialize(StdVector(StdString.(opts)))
+end
 
-    # Provides shortcuts for MultiIndexSet for Julia-style arrays
-    MultiIndexSet(A::AbstractMatrix{<:Integer}) = MultiIndexSet(Cint.(collect(A)))
-    MultiIndexSet(A::AbstractVector{<:Integer}) = MultiIndexSet(Cint.(collect(reshape(A, length(A), 1))))
-    Base.getindex(A::MultiIndex, i::AbstractVector{<:Integer}) = getindex.((A,), i)
-    Base.lastindex(A::MultiIndex) = length(A)
+function Base.iterate(mset::MultiIndexSet)
+    Size(mset) < 1 && return nothing
+    mset[1],2
+end
+
+function Base.iterate(mset::MultiIndexSet,state::Int)
+    state > Size(mset) && return nothing
+    return mset[state], state+1
+end
+
+"""
+    MultiIndexSet(A::AbstractVecOrMat{<:Integer})
+
+Create a set of MultiIndices from the rows of `A`.
+
+These indices represent a basis for a multivariate expansion or,
+further, monotone expansion. Each element of the set is a MultiIndex
+representing one basis function via the degrees in each dimension.
+
+# Example
+```jldoctest
+julia> # Functions like: c_1xy^2z^3 + c_2xyz + c_3
+
+julia> A = [1 2 3;1 1 1; 0 0 0];
+
+julia> mset = MultiIndexSet(A);
+```
+
+See also[`MultiIndex`](@ref), [`FixedMultiIndex`](@ref), [`Fix`](@ref)
+"""
+MultiIndexSet(A::AbstractMatrix{<:Integer}) = MultiIndexSet(Cint.(collect(A)))
+MultiIndexSet(A::AbstractVector{<:Integer}) = MultiIndexSet(Cint.(collect(reshape(A, length(A), 1))))
+Base.getindex(A::MultiIndex, i::AbstractVector{<:Integer}) = getindex.((A,), i)
+Base.lastindex(A::MultiIndex) = length(A)
 
     Base.getindex(A::CxxWrap.reference_type_union(MParT.TriangularMap), s::Base.UnitRange) = Slice(A, first(s), last(s))
 
-    """
-        `MapOptions(;kwargs...)`
-    Takes the fields from MParT's `MapOptions` as keyword arguments, and
-    assigns the field value based on a String from the kwarg value, e.g.
-    ```julia
-    julia> using MParT
+"""
+    MapOptions(;kwargs...)
+Takes the fields from MParT's `MapOptions` as keyword arguments, and
+assigns the field value based on a String from the kwarg value.
 
-    julia> MapOptions(basisType="HermiteFunctions")
-    ```
-    """
-    function MapOptions(;kwargs...)
-        opts = __MapOptions()
-        for kwarg in kwargs
-            field = Symbol("__"*string(first(kwarg))*"!")
-            value = MParT.eval(Meta.parse("__"*last(kwarg)))
-            getfield(MParT, field)(opts, value)
+# Arguments
+- ``
+
+# Example
+```jldoctest
+julia> using MParT
+
+julia> MapOptions(basisType="HermiteFunctions", basisLB=-3., basisUB=3.)
+```
+"""
+function MapOptions(;kwargs...)
+    opts = __MapOptions()
+    for kwarg in kwargs
+        field = Symbol("__"*string(first(kwarg))*"!")
+        value = last(kwarg)
+        if value isa String
+            value = MParT.eval(Meta.parse("__"*value))
         end
-        opts
+        getfield(MParT, field)(opts, value)
     end
+    opts
+end
 
-    """
-        `ComposedMap(maps::Vector)`
-    Creates a `ComposedMap` from a vector of `ConditionalMapBase` objects.
-    """
-    function ComposedMap(maps::Vector{<:CxxWrap.StdLib.SharedPtr{<:ConditionalMapBase}})
-        maps_cmb = Vector{CxxWrap.StdLib.SharedPtr{ConditionalMapBase}}(undef, length(maps))
-        for (i, map) in enumerate(maps)
-            maps_cmb[i] = map
-        end
-        maps_std = StdVector(maps_cmb)
-        ComposedMap(maps_std)
+"""
+    `TrainOptions(;kwargs...)`
+
+Takes the fields from MParT's `TrainOptions` as keyword arguments, and
+assigns the field value based on a String from keyword arguments.
+# Arguments
+-``:
+
+# Examples
+```jldoctest
+julia> TrainOptions(opt_alg="LD_SLSQP")
+```
+"""
+function TrainOptions(;kwargs...)
+    opts = __TrainOptions()
+    for kwarg in kwargs
+        field = Symbol("__"*string(first(kwarg))*"!")
+        value = last(kwarg)
+        getfield(MParT, field)(opts, value)
     end
+    opts
+end
 
-    # MultiIndex-related exports
-    export MultiIndex, MultiIndexSet, FixedMultiIndexSet
-    export Fix, CreateTotalOrder, Size
-    # ParameterizedFunctionBase-related exports
-    export CoeffMap, SetCoeffs, numCoeffs, inputDim, outputDim
-    export Evaluate, CoeffGrad, Gradient
-    # ConditionalMapBase-related exports
-    export GetBaseFunction, LogDeterminant, LogDeterminantCoeffGrad, Inverse
-    # TriangularMap-related exports
-    export InverseInplace, GetComponent
-    # AffineMap-related exports
-    export AffineMap, AffineFunction
-    # ComposedMap-related exports
-    export ComposedMap
-    # MapFactory-related exports
-    export CreateComponent, CreateTriangular
-    # MapOptions-related exports
-    export MapOptions
-    # Other important utils
-    export Concurrency
+"""
+    CreateGaussianKLObjective(train)
+    CreateGaussianKLObjective(train, outputDim)
+    CreateGaussianKLObjective(train, test)
+    CreateGaussianKLObjective(train, test, outputDim)
+
+Create an objective for the optimization problem of training a
+transport map.
+
+Currently only supports variational simulation-based inference, i.e.
+creating a map that uses samples `train` from target distribution and a
+Gaussian reference distribution of dimension `outputDim`. If `outputDim`
+is zero, this is equivalent to when `outputDim == size(train,1)`.
+
+# Arguments
+- `train::Matrix{Float64}`: mandatory training dataset
+- `test::Matrix{Float64}`: optional test dataset
+- `outputDim::Int = 0`: Dimensions of output of map
+
+# Examples
+```jldoctest
+julia> using Random; rng = Random.Xoshiro(1);
+
+julia> # Replace RNG type with MersenneTwister for <1.7
+
+julia> N, inDim, outDim = 1000, 4, 2;
+
+julia> samples = 2*randn(rng, inDim, N) .+ 5;
+
+julia> obj1 = CreateGaussianKLObjective(samples);
+
+julia> obj2 = CreateGaussianKLObjective(samples, outDim);
+
+julia> train, test = samples[:,1:500], samples[:,501:end];
+
+julia> obj3 = CreateGaussianKLObjective(train, test);
+
+julia> obj4 = CreateGaussianKLObjective(train, test, outDim);
+```
+"""
+CreateGaussianKLObjective
+
+CreateGaussianKLObjective(train::Matrix{Float64}) = CreateGaussianKLObjective(train,0)
+CreateGaussianKLObjective(train::Matrix{Float64},test::Matrix{Float64}) = CreateGaussianKLObjective(train,test,0)
+
+"""
+    `ATMOptions(;kwargs...)`
+
+Takes the fields from MParT's `ATMOptions` as keyword arguments, and
+assigns the field value based on a String from keyword arguments.
+
+# Examples
+```jldoctest
+julia> maxDegrees = MultiIndex(2,3) # limit both dimensions by order 3
+
+julia> ATMOptions(opt_alg="LD_SLSQP", maxDegrees=maxDegrees)
+```
+"""
+function ATMOptions(;kwargs...)
+    opts = __ATMOptions()
+    for kwarg in kwargs
+        field = Symbol("__"*string(first(kwarg))*"!")
+        value = last(kwarg)
+        getfield(MParT, field)(opts, value)
+    end
+    opts
+end
+
+# To print MapOptions, TrainOptions objects
+Base.show(io::IO,::MIME"text/plain", opts::__MapOptions) = print(io,string(opts))
+Base.show(io::IO,::MIME"text/plain", opts::__TrainOptions) = print(io,string(opts))
+
+
+"""
+    DeserializeMap(filename::String)
+REQUIRES CEREAL INSTALLATION. Deserializes a map and returns its input dimension, output dimension, and coefficient.
+"""
+function DeserializeMap(filename::String)
+    dims = Cint[0,0]
+    coeffs = __DeserializeMap(filename, dims);
+    dims[1], dims[2], coeffs
+end
+
+
+"""
+    TrainMapAdaptive(msets, objective, options)
+This implements the ATM algorithm (Baptista et al. 2022), which takes in
+an initial guess of multiindex sets for each output dimension and adapts
+those sets to better suit the probability distribution.
+
+# Examples
+"""
+function TrainMapAdaptive(msets::Vector{<:MultiIndexSet},obj::CxxWrap.StdLib.SharedPtr{<:MapObjective}, opts::__ATMOptions)
+    msets_vec = [CxxRef(mset) for mset in msets]
+    TrainMapAdaptive(msets_vec, obj, opts)
+end
+
+
+"""
+    `TriangularMap(maps::Vector)`
+Creates a `TriangularMap` from a vector of `ConditionalMapBase` objects.
+
+# Examples
+```jldoctest
+julia> dim, order = 5, 3
+
+julia> msets = [FixedMultiIndexSet(d, order) for d in 1:dim]
+
+julia> opts = MapOptions()
+
+julia> components = [CreateComponent(mset, opts) for mset in msets]
+
+julia> trimap = TriangularMap(components)
+```
+"""
+function TriangularMap(maps::Vector)
+    maps = StdVector([map for map in maps])
+    TriangularMap(maps)
+end
+
+"""
+    `ComposedMap(maps::Vector)`
+Creates a `ComposedMap` from a vector of `ConditionalMapBase` objects.
+""" # TODO: Example
+function ComposedMap(maps::Vector{<:ConditionalMapBasePtr})
+    maps_std = StdVector([map for map in maps])
+    ComposedMap(maps_std)
+end
+
+"""
+    MultiIndex(A::AbstractVector{<:Int})
+MultiIndex defines the order of one term of a function expansion for each dimensions.
+
+# Example
+```jldoctest
+julia> degrees = [3,2,1]; # Represents polynomial basis function similar to x^3y^2z^1
+
+julia> midx = MultiIndex(degrees);
+
+julia> midx[3]
+0x00000001
+```
+See also [`MultiIndexSet`](@ref), [`FixedMultiIndexSet`](@ref), [`Fix`](@ref)
+"""
+MultiIndex(A::AbstractVector{<:Int}) = MultiIndex(StdVector(Cuint.(A)))
+
+# MultiIndex-related exports
+export MultiIndex, MultiIndexSet, FixedMultiIndexSet
+export Fix, CreateTotalOrder, Size
+# ParameterizedFunctionBase-related exports
+export CoeffMap, SetCoeffs, numCoeffs, inputDim, outputDim
+export Evaluate, CoeffGrad, Gradient
+# ConditionalMapBase-related exports
+export ConditionalMapBasePtr, GetBaseFunction, LogDeterminant, LogDeterminantCoeffGrad, Inverse
+# TriangularMap-related exports
+export TriangularMap, InverseInplace, GetComponent
+# AffineMap-related exports
+export AffineMap, AffineFunction
+# ComposedMap-related exports
+export ComposedMap
+# MapFactory-related exports
+export CreateComponent, CreateTriangular
+# MapOptions-related exports
+export MapOptions
+# Serialization-related exports
+export Serialize, Deserialize, DeserializeMap
+# Map training related exports
+export CreateGaussianKLObjective, TrainOptions, TrainMap, TestError
+# ATM-related exports
+export TrainMapAdaptive, ATMOptions
+# Other important utils
+export Concurrency
 end
